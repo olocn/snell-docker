a2a9d565f846ef43b55985d59b0e584ec69000da
Civi <civi@rcg.fun>
2025-11-19 10:37:05 +0800

    Update entrypoint.sh
    Bump snell-server version to 5.0.1

--- entrypoint.sh
+++ entrypoint.sh
@@ -32,7 +32,7 @@ EOF
 }
 
 download_snell() {
-  VERSION=${VERSION:-v5.0.0}
+  VERSION=${VERSION:-v5.0.1}
   case "${TARGETPLATFORM}" in
     "linux/amd64") SNELL_URL="https://dl.nssurge.com/snell/snell-server-${VERSION}-linux-amd64.zip" ;;
     "linux/386") SNELL_URL="https://dl.nssurge.com/snell/snell-server-${VERSION}-linux-i386.zip" ;;
